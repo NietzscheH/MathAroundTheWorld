4616c778f52d57026eb0a1aabb528c669980df3c
Yuqiao <hyq0401@outlook.com>
2019-07-24 08:16:20 -0400

    Now it's able to create boxes with messages

--- QuestionBox.py
+++ QuestionBox.py
@@ -1,15 +1,34 @@
 import pygame as pg
 
 class QuestionBox:
-    def __init__(self, xcor, y_speed, scre, width=30, height=20, color=(0,0,0)):
+    def __init__(self, xcor, y_speed, scre, text='Hello', text_color=(0,0,0), bg_color=(255,255,255)):
         self.x = xcor
-        self.y = -height
+        self.y = None
+        self.h = None
         self.s = y_speed
-        self.w = width
-        self.h = height
+        self.txt = text
         self.win = scre
-        self.color = color
-    
-    def drawMyself(self):
+        self.screen_size = self.win.get_rect().size
+        self.text_color = text_color
+        self.bg_color = bg_color
+
+        # first create an image with the message
+        f = pg.font.SysFont(None, 24)
+        self.msg_image = f.render(self.txt, True, self.bg_color, self.text_color)
+        # then create a rect object of that image
+        self.msg_image_rect = self.msg_image.get_rect()
+        self.h = self.msg_image_rect.size[1]
+        self.y = -self.h
+
+    def _drawMyself_(self):
+        # draw a plain black box
         pg.draw.rect(self.win, self.color, (self.x, self.y, self.w, self.h))
+        self.y += self.s
+
+    def drawMyself(self):
+        # create a box with message
+        self.msg_image_rect.topleft = (self.x, self.y)
+        # display the image in the rect object and show them on the screen
+        self.win.blit(self.msg_image, self.msg_image_rect)
+
         self.y += self.s
--- controller.py
+++ controller.py
@@ -9,15 +9,22 @@ class Controller:
         self.question_list = []
         self.count = 0
         self.lives = 3
-        self.density = 2000
-        self.speed = 0.02
+        self.density = 1500
+        self.speed = 0.05
         
-    def createQuestionBox(self, x=None, s=0.02):
+    def createQuestionBox(self):
         # create objects here
         x = random.randrange(50, 751)
-        self.question_list.append(QB(x, s, self.screen))
+        self.question_list.append(QB(x, self.speed, self.screen))
         self.count += 1
-        
+    
+    def deleteOutscreenBox(self):
+        # delete boxes that are out of the screen
+        for i in self.question_list[:]:
+            if i.y > i.screen_size[1]-i.h:
+                self.question_list.remove(i)
+                self.count -= 1
+
     def initialize(self):
         # initialize a screen
         pg.init()
@@ -34,13 +41,14 @@ class Controller:
             
             self.screen.fill((255, 255, 255))
 
-            if d == self.density:
+            if d == self.density: # the bigger the density is, the slower the game goes
                 self.createQuestionBox()
                 d = 0
             d += 1
-
+            
             for i in self.question_list:
                 i.drawMyself()
+            self.deleteOutscreenBox()
 
             pg.display.update()
 
