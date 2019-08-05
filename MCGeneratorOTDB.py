1cd12041531ec827a49aad13d6c60c0d2261f2de
Kimberly Meeker <kmeeker2@binghamton.edu>
2019-08-05 07:02:01 -0400

    Updating MCGeneratorOTDB.py

--- MCGeneratorOTDB.py
+++ MCGeneratorOTDB.py
@@ -1,7 +1,7 @@
 import requests
 import random
 
-class MCGenerator:
+class MCGeneratorOTDB:
     '''
         This Proxy class supplies multiple choice questions from opentdb.com
     '''
@@ -21,103 +21,79 @@ class MCGenerator:
         # Converts json to Python dictionary
         question_data = question_data.json()
         self.data_list = question_data['results']
-        data_dict = self.data_list[0]
+        self.data_dict = self.data_list[0]
 
         # Creates a list of answers
-        self.aquestion = data_dict['question']
-        answer_list = [data_dict['correct_answer']]
-        for i in data_dict['incorrect_answers']:
+        self.aquestion = self.data_dict['question']
+        answer_list = [self.data_dict['correct_answer']]
+        for i in self.data_dict['incorrect_answers']:
             answer_list.append(i)
 
         # Assigns answers randomly
-        self.mc_list = random.sample(answer_list, 4)
+        random_list = random.sample(answer_list, 4)
+        self.choice_a = "a) " + random_list[0]
+        self.choice_b = "b) " + random_list[1]
+        self.choice_c = "c) " + random_list[2]
+        self.choice_d = "d) " + random_list[3]
+
+        self.mc_list = [self.choice_a, self.choice_b, self.choice_c, self.choice_d]
+
+        # Identify correct answer
+        for choice in self.mc_list:
+            if self.data_dict['correct_answer'] in choice:
+                correct_choice = choice
+        self.ans = correct_choice
 
 
     def showMCQuestion(self):
         '''
             Prints the question and four optional responses
             args: none
-            return: none
+            return: ('str') the correct answer to the question
         '''
 
         print(self.aquestion)
-        print('a)', self.mc_list[0])
-        print('b)', self.mc_list[1])
-        print('c)', self.mc_list[2])
-        print('d)', self.mc_list[3])            
+        print(self.choice_a)
+        print(self.choice_b)
+        print(self.choice_c)
+        print(self.choice_d)
+
+        return self.ans
+                   
         
 def test():
-    
-    question_data = requests.get('https://opentdb.com/api.php?amount=1&category=19&difficulty=easy&type=multiple')
-
-    print(type(question_data))
-    question_data = question_data.json()
-
-    print(question_data)
-    print(type(question_data))
-
-    data_list = question_data['results']
-
-    print(data_list)
-    print(type(data_list))
-
-    data_dict = data_list[0]
-    print(data_dict)
-    print(type(data_dict))
-
-    # Isolates question
-    aquestion = data_dict['question']
-    print(aquestion)
-    
-    # Creates a list of answers
-    answer_list = [data_dict['correct_answer']]
-    
-    for i in data_dict['incorrect_answers']:
-        answer_list.append(i)
-    print(answer_list)
-
-    # Sorts for multiple choice
-    mc_list = [random.choice(answer_list) for i in answer_list]
-
-    print(aquestion)
-    print('1)', mc_list[0])
-    print('2)', mc_list[1])    
-    print('3)', mc_list[2])
-    print('4)', mc_list[3])
-
-    
-
-
-def test2():
-    print('########### Testing MCGenerator Level 1 ##############')
+    print('########### Testing MCGeneratorOTDB Level 1 ##############')
     try:
-        math_question = MCGenerator('level1')
+        math_question = MCGeneratorOTDB('level1')
     except:
         print("Get questions from the local bank.")
     else:
         math_question.showMCQuestion()
+        print('Answer: ', math_question.ans)
         #print(math_question.data_list)
 
-    print('########### Testing MCGenerator Level 2 ##############')    
+    print('########### Testing MCGeneratorOTDB Level 2 ##############')    
     try:
-        math_question = MCGenerator('level2')
+        math_question = MCGeneratorOTDB('level2')
     except:
         print("Get questions from the local bank.")
     else:
         math_question.showMCQuestion()
+        print('Answer: ', math_question.ans)
         #print(math_question.data_list)
 
-    print('########### Testing MCGenerator Level 3 ##############') 
+    print('########### Testing MCGeneratorOTDB Level 3 ##############') 
     try:
-        math_question = MCGenerator('level3')
+        math_question = MCGeneratorOTDB('level3')
     except:
         print("Get questions from the local bank.")
     else:
         math_question.showMCQuestion()
+        print('Answer: ', math_question.ans)
         #print(math_question.data_list)
 
-    print('############ MCGenerator Tests Complete ##############')
+    print('############ MCGeneratorOTDB Tests Complete ##############')
 
 if __name__ == '__main__':
 
-    test2()
+    test()
